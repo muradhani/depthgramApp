7568b64c5f1c87319faf8bcc891994411e6e7708
muradhani <mouradhany574@gmail.com>
2025-09-16 14:14:05 +0300

    hide bottom bar and status bar

--- composeApp/src/androidMain/kotlin/org/carftaura/depthgramapp/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/carftaura/depthgramapp/MainActivity.kt
@@ -6,16 +6,25 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import org.carftaura.depthgramapp.utils.AndroidContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+        hideSystemBars()
         AndroidContext.appContext = applicationContext
         setContent {
             App()
         }
+    }
+    private fun hideSystemBars() {
+        val controller = WindowInsetsControllerCompat(window, window.decorView)
+        controller.hide(WindowInsetsCompat.Type.systemBars())
+        controller.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
     }
 }
 
