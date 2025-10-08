e516806db3a788412d19c78d5caee1ab3d2e9f80
muradhani <mouradhany574@gmail.com>
2025-10-08 20:14:30 +0300

    remove depth estimator and related dependencies

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -33,21 +33,7 @@ kotlin {
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
-            implementation("androidx.camera:camera-core:1.3.1")
-            implementation("androidx.camera:camera-lifecycle:1.3.1")
-            implementation("androidx.camera:camera-camera2:1.3.1")
-            implementation("androidx.camera:camera-view:1.3.1")
-            implementation("androidx.camera:camera-extensions:1.3.1")
-            implementation("io.ktor:ktor-client-android:3.2.2")
-            implementation("org.tensorflow:tensorflow-lite:2.12.0")
-            implementation("org.tensorflow:tensorflow-lite-support:0.4.3")
-            implementation("org.tensorflow:tensorflow-lite-gpu:2.13.0")
-            implementation("org.tensorflow:tensorflow-lite:+")
-            implementation("org.tensorflow:tensorflow-lite-gpu:+")
-            implementation("org.tensorflow:tensorflow-lite-gpu-api:+")
-            implementation("org.tensorflow:tensorflow-lite-gpu-delegate-plugin:+")
-            implementation("org.tensorflow:tensorflow-lite-support:+")
-            implementation("com.google.ar:core:1.41.0")
+            implementation("com.google.ar:core:1.50.0")
             implementation(libs.core)
 // or latest version
         }
